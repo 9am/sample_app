eeefc117f5cf075b694d427a8b7bc912ea0b6cc7
Bo Gao <bgao@freewheel.tv>
2022-02-02 12:39:36 +0800

    test

--- _actions/writefile.js
+++ _actions/writefile.js
@@ -4,7 +4,8 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = async ({ github, context, core }) => {
     const pages = JSON.parse(process.env.DATA);
     core.info(`write start: ${pages.length}`);
-    exec('rm ./docs/data/*')
+    await exec('mkdir ./docs/data');
+    await exec('rm ./docs/data/*');
     await Promise.all(pages.map((page, index) => exec(`echo '${JSON.stringify(page, null, 2)}' > ./docs/data/${index}.json`)));
     core.info(`write end: ${pages.length}`);
 }
